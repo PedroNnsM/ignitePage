34f3c2fd45f3ee4aa317694431e31b8702543ac4
PedroNnsM <pedronunes95@outlook.com>
2022-06-26 15:56:54 -0300

    Mutation api

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4p0jgv41i7h01xs324i4upm/master',
-    cache: new InMemoryCache()
-})
+	uri: import.meta.env.VITE_API_URL,
+	headers: {
+		"Authorization": `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+	},
+	cache: new InMemoryCache()
+});
